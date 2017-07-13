897ef2b2e7ecc9fe09af495d47c08eb62fe259a9
Evan Sitzes - Bellevue <esitzes@esitzesmac.local>
2017-07-13 16:54:02 -0700

    add network speed recording script

---
+++ record-network-speed.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERNAME=pi
+PORT=3336
+HOSTNAME=sitzilla.mynetgear.com
+SCRIPT="date >> network-speed-record"
+
+check_connection() {
+	if [ $? -ne 0 ]; then
+		echo "SSH error into pi"
+	else
+		echo "PI connection successful"
+	fi
+}
+
+# Write the current date to the Raspberry Pi
+ssh -o ConnectTimeout=10 -l ${USERNAME} -p ${PORT} ${HOSTNAME} "${SCRIPT}"
+check_connection
+
+# Check network speed and write it to the Raspberry Pi
+SCRIPT="speedtest-cli | grep -iE \"Download:|Upload:\" >> network-speed-record"
+ssh -o ConnectTimeout=60 -l ${USERNAME} -p ${PORT} ${HOSTNAME} "${SCRIPT}"
+check_connection
